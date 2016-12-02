59f87692083f0dcba035d002ffe58f0a84df4317
Morgan <morganrozman@yahoo.com>
2016-12-02 12:43:50 +0100

    FriendActivity/FriendAdapter/Firebase JSON

--- app/src/main/java/hu/ait/android/mobilefinalproject/adapter/FriendRecyclerAdapter.java
+++ app/src/main/java/hu/ait/android/mobilefinalproject/adapter/FriendRecyclerAdapter.java
@@ -1,46 +1,53 @@
-//package hu.ait.android.mobilefinalproject.adapter;
-//
-//import android.support.v7.widget.RecyclerView;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-//import java.util.List;
-//
-//import hu.ait.android.mobilefinalproject.data.Friend;
-//
-///**
-// * Created by Morgan on 12/1/2016.
-// */
-//public class FriendRecyclerAdapter extends RecyclerView.Adapter<FriendRecyclerAdapter.ViewHolder> {
-//
-//    private List<Friend> friendList;
-//
-//    public FriendRecyclerAdapter() {
-//        friendList = ;
-//
-//    }
-//
-//    @Override
-//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        return null;
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(ViewHolder holder, int position) {
-//
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return 0;
-//    }
-//
-//    public static class ViewHolder extends RecyclerView.ViewHolder  {
-//
-//        public ViewHolder(final View itemView) {
-//            super(itemView);
-//
-//        }
-//    }
-//
-//}
+package hu.ait.android.mobilefinalproject.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hu.ait.android.mobilefinalproject.R;
+import hu.ait.android.mobilefinalproject.data.Friend;
+
+/**
+ * Created by Morgan on 12/1/2016.
+ */
+
+public class FriendRecyclerAdapter extends RecyclerView.Adapter<FriendRecyclerAdapter.ViewHolder> {
+
+    private List<Friend> friendList;
+
+    public FriendRecyclerAdapter() {
+        friendList = new ArrayList<Friend>();
+        friendList.add(new Friend("FriendName", 600, 200));
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View friendRow = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.friend_row, parent, false);
+
+        return new ViewHolder(friendRow);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return friendList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder  {
+
+        public ViewHolder(final View itemView) {
+            super(itemView);
+
+        }
+    }
+
+}
