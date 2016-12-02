e8275cb6e5bc22dc8d8d1c2bb129ebb4ffdf8666
Sam Sheppe <ssheppe63@gmail.com>
2016-12-02 12:37:35 +0100

    sam's changes

--- app/src/main/java/hu/ait/android/mobilefinalproject/AccountActivity.java
+++ app/src/main/java/hu/ait/android/mobilefinalproject/AccountActivity.java
@@ -2,13 +2,52 @@ package hu.ait.android.mobilefinalproject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
-public class AccountActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AccountActivity extends Activity{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+
+        ButterKnife.bind(this);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
     }
+
+    @OnClick(R.id.tvEditPassword)
+    void editPassword(){
+        Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.tvUserFriendsAmount)
+    void openFriendsActivity(){
+
+    }
+
+    @OnClick(R.id.tvLogout)
+    void logout(){
+
+    }
+
+
 
 }
