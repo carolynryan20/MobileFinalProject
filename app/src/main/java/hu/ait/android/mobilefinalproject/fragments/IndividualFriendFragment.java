dae78dd23f71aaefca6028c6755fd7d592039b07
Sam Sheppe <ssheppe63@gmail.com>
2016-12-02 21:14:28 +0100

    individual friend xml

---
+++ app/src/main/java/hu/ait/android/mobilefinalproject/fragments/IndividualFriendFragment.java
@@ -0,0 +1,13 @@
+package hu.ait.android.mobilefinalproject.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndividualFriendFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_friend_fragment);
+    }
+}
