b2b02de22af4dd5aea6d70274b30041be1aa33aa
Carolyn Ryan <ryanc2@carleton.edu>
2016-12-03 14:25:05 +0100

    Friends Recycler View Stuff

--- app/src/main/java/hu/ait/android/mobilefinalproject/fragments/FriendsFragment.java
+++ app/src/main/java/hu/ait/android/mobilefinalproject/fragments/FriendsFragment.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,9 +35,7 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_friends, container, false);
-
-//        ViewPager pager = (ViewPager) root.findViewById(R.id.pager);
-//        pager.setAdapter(new MainPagerAdapter(this.getFragmentManager(), getContext()));
+//        setupRecyclerView();
 
         return root;
     }
@@ -44,6 +43,8 @@ public class FriendsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        setupRecyclerView();
+
 
 //        FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +83,17 @@ public class FriendsFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void setupRecyclerView() {
+        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerItem);
+        recyclerView.setHasFixedSize(true);
+        final LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        friendRecyclerAdapter = new FriendRecyclerAdapter();
+
+        recyclerView.setAdapter(friendRecyclerAdapter);
     }
 
 }
