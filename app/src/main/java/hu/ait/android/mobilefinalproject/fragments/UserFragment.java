eb42e0e451c0a3e43e95c680e40107b189f4afcb
Sam Sheppe <ssheppe63@gmail.com>
2016-12-04 22:06:23 +0100

    user xml formatting good

--- app/src/main/java/hu/ait/android/mobilefinalproject/fragments/UserFragment.java
+++ app/src/main/java/hu/ait/android/mobilefinalproject/fragments/UserFragment.java
@@ -5,13 +5,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import hu.ait.android.mobilefinalproject.R;
-import hu.ait.android.mobilefinalproject.adapter.FriendRecyclerAdapter;
 
 /**
  * Created by ssheppe on 12/3/16.
@@ -30,7 +28,7 @@ public class UserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        root = inflater.inflate(R.layout.activity_account, container, false);
+        root = inflater.inflate(R.layout.fragment_user, container, false);
 
 //        ViewPager pager = (ViewPager) root.findViewById(R.id.pager);
 //        pager.setAdapter(new MainPagerAdapter(this.getFragmentManager(), getContext()));
