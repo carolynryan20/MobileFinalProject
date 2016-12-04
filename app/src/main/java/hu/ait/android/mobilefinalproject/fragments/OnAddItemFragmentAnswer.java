d81ddbddeb4b4c64e3f7bb6d366da84b086ccf2b
Morgan <morganrozman@yahoo.com>
2016-12-04 14:54:05 +0100

    FAB Moved/clumps on firebase

--- app/src/main/java/hu/ait/android/mobilefinalproject/fragments/OnAddItemFragmentAnswer.java
+++
@@ -1,9 +0,0 @@
-package hu.ait.android.mobilefinalproject.fragments;
-
-/**
- * Created by Morgan on 12/4/2016.
- */
-public interface OnAddItemFragmentAnswer {
-    public void onPositiveSelected();
-    public void onNegativeSelected();
-}
