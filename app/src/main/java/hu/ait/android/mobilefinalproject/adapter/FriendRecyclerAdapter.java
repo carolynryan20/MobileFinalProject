0cc37bcba2eff793e1654b7a3afd4d88129eef4d
Morgan <morganrozman@yahoo.com>
2016-12-09 12:44:43 +0100

    friends debt/owed card display werkin'

--- app/src/main/java/hu/ait/android/mobilefinalproject/adapter/FriendRecyclerAdapter.java
+++ app/src/main/java/hu/ait/android/mobilefinalproject/adapter/FriendRecyclerAdapter.java
@@ -40,14 +40,8 @@ public class FriendRecyclerAdapter extends RecyclerView.Adapter<FriendRecyclerAd
 
     public FriendRecyclerAdapter(Context context) {
         friendList = new ArrayList<Friend>();
-//        friendList.add(new Friend("FriendName", 600, 200));
-        //this.clumpList = myUser.getClumps();
         this.friendKeys = new ArrayList<String>();
-
         this.context = context;
-
-//        clumpsRef = FirebaseDatabase.getInstance().getReference("users")
-//                .getRef().child(uid).child("clumps");
 
         checkActivityImplementsResponseInterface();
     }
@@ -68,57 +62,59 @@ public class FriendRecyclerAdapter extends RecyclerView.Adapter<FriendRecyclerAd
         return rcv;
     }
 
-    //    @Override
-//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View friendRow = LayoutInflater.from(parent.getContext()).inflate(
-//                R.layout.friend_row, parent, false);
-//
-//
-//        return new ViewHolder(friendRow);
-//    }
-
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         final String currentUsername = friendList.get(holder.getAdapterPosition()).getUsername();
         holder.tvFriendUsername.setText(currentUsername);
 
+        setUpBackFriendsCard(holder, currentUsername);
+
+    }
+
+    private void setUpBackFriendsCard(final ViewHolder holder, final String currentUsername) {
         DatabaseReference friendsRef = FirebaseDatabase.getInstance().getReference().child("users")
                 .child(FriendsFragment.getUid()).child("clumps");
         Query friendsQuery = friendsRef.orderByChild("owedUser");
         friendsQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // create the friend/owed map
-                Map<String, Double> owedMap = new HashMap<String, Double>();
                 for (DataSnapshot ss : dataSnapshot.getChildren()) {
-//                    Toast.makeText(context, ss.toString(), Toast.LENGTH_SHORT).show();
                     String owedUser = (String) ss.child("owedUser").getValue();
                     if(owedUser.equals(FriendsFragment.getUserName())) {
-                        List<Double> debtList = new ArrayList<Double>();
-                        int numDebtUsers = (int) ss.child("debtUsers").getChildrenCount();
-                        for (DataSnapshot debtUser : ss.child("debtUsers").getChildren()) {
-                            if (debtUser.getKey().toString().equals(currentUsername)) {
-                                String debtUserValue = debtUser.getValue().toString();
-                                Double debtUserDouble = Double.parseDouble(debtUserValue);
-
-                                debtList.add(debtUserDouble);
-//                                totalOwed += debtUserDouble;
-//                                Toast.makeText(context, "" + totalOwed, Toast.LENGTH_SHORT).show();
-//                                holder.tvFriendDebt.setText(totalOwed.toString());
-                            }
-                        }
-                        // try iterating through list to sum all values and set to textview
-//                        holder.tvFriendDebt.setText();
+                        getFriendOwed(ss, currentUsername, holder);
+                    }
+                    else if (owedUser.equals(currentUsername)){
+                        getFriendDebt(ss, holder);
                     }
                 }
             }
-
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
+            public void onCancelled(DatabaseError databaseError) {}
         });
+    }
 
+    private void getFriendDebt(DataSnapshot ss, ViewHolder holder) {
+        for (DataSnapshot debtUser : ss.child("debtUsers").getChildren()) {
+            if (debtUser.getKey().toString().equals(FriendsFragment.getUserName())) {
+                String debtUserValue = debtUser.getValue().toString();
+                Double debtUserDouble = Double.parseDouble(debtUserValue);
+
+                Double prevDebt = Double.valueOf(holder.tvFriendDebt.getText().toString());
+                holder.tvFriendDebt.setText(String.format("%1$.2f", (prevDebt+debtUserDouble)));
+            }
+        }
+    }
+
+    private void getFriendOwed(DataSnapshot ss, String currentUsername, ViewHolder holder) {
+        for (DataSnapshot debtUser : ss.child("debtUsers").getChildren()) {
+            if (debtUser.getKey().toString().equals(currentUsername)) {
+                String debtUserValue = debtUser.getValue().toString();
+                Double debtUserDouble = Double.parseDouble(debtUserValue);
+
+                Double prevDebt = Double.valueOf(holder.tvFriendOwed.getText().toString());
+                holder.tvFriendOwed.setText(String.format("%1$.2f", (prevDebt+debtUserDouble)));
+            }
+        }
     }
 
     @Override
@@ -144,7 +140,6 @@ public class FriendRecyclerAdapter extends RecyclerView.Adapter<FriendRecyclerAd
         if (!friendKeys.contains(key)) {
             friendList.add(0, friend);
             friendKeys.add(0, key);
-            // refresh the whole list
             notifyDataSetChanged();
         }
     }
